3c326c97583ea3e2dd5477f13e6530dc4c997574
JACQUES sandra <ssephiha1905@gmail.com>
2022-03-02 18:21:56 +0100

    upload projet

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const Sauce = require('./models/sauce');
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.abv3n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  app.use(express.json());
+  app.use('/api/sauces', saucesRoutes);
+  app.use('/api/auth', userRoutes);
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
+
